58338bada5d13be60d1f5b63d3a6abf1f1ab6f48
parilov alexx <parilov_alexx@mail.ru>
2024-04-23 20:08:10 +0300

    css link deleted & e2e test edited

---
+++ e2e/e2e.test.js
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Форма popover', () => {
+  let browser;
+  let page;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({
+      //headless: false, // show gui
+      //slowMo: 150,
+      //devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('должен проверить открытие окна popover', async () => {
+    await page.goto(baseUrl);
+    const btn = await page.$('button[class=button]');
+    btn.click();
+    await page.waitForSelector('div.popover');
+  });
+});
